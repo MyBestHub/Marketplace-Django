ccee897a08c81f08ea1871805d5c78f200ded5a3
Hussein Dabs <fifa_street_3000@hotmail.com>
2023-04-05 07:51:39 -0400

    Key change

--- Marketplace/Marketplace/settings.py
+++ Marketplace/Marketplace/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_j870%d1ov&bc3#n$kx!^xfh)^1nh$fc2lb3wb0g&%@dyr0*(t'
+SECRET_KEY = 'django-insecure-_
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
